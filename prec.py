79a399f0c4c1df7f6198ed3f5ed253e0c499820e
mazucanti <daniel.mazucanti.ismart@gmail.com>
2020-12-15 06:45:20 -0300

    Reformatação de código

--- prec.py
+++ prec.py
@@ -11,25 +11,28 @@ for arquivo in arquivos:
     with open(arquivo, 'r') as fp:
         for i, linha in enumerate(fp):
             if i != 0:
-                linha = linha.replace("-9.96921e+36", "0")
-                linha = linha.split()
-                linha[1] = round(float(linha[1]))
-                linha[2] = round(float(linha[2]))
-                linha[3] = round(float(linha[3]))
-                l_final.append(linha)
+                if linha == "":
+                    pass
+                else:
+                    linha = linha.replace("-9.96921e+36", "0")
+                    linha = linha.split()
+                    linha[1] = round(float(linha[1]))
+                    linha[2] = round(float(linha[2]))
+                    linha[3] = round(float(linha[3]))
+                    l_final.append(linha)
 
-                if l_final[i-1][0] == l_final[i-2][0] and l_final[i-1][1] == l_final[i-2][1] and l_final[i-1][2] == l_final[i-2][2]:
-                    l_final[i-1][3] = float((l_final[i-2][3] + l_final[i-1][3]) / 2)
-                    l_final[i-2] = "-"
-
+                    if l_final[i - 1][0] == l_final[i - 2][0] and l_final[i - 1][1] == l_final[i - 2][1] and \
+                            l_final[i - 1][2] == l_final[i - 2][2]:
+                        l_final[i - 1][3] = float((l_final[i - 2][3] + l_final[i - 1][3]) / 2)
+                        l_final[i - 2] = "-"
 
     with open('saídas/%s' % arquivo.name, 'w') as fp:
         for linha in l_final:
             if type(linha) == list:
                 linha = tuple(linha)
                 to_fp = "%s,%s,%s,%s" % linha
-            
+
             else:
                 continue
 
-            fp.write(to_fp+'\n')
+            fp.write(to_fp + '\n')
